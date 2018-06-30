55c087e43cb22025d281e7381e8f5c356671a8ed
Oliver Ellmers <hi@oliverellmers.com>
2018-06-30 17:24:50 +0100

    UI Updates
    Animations all in - Ready to get under the hood

---
+++ Assets/Scripts/PlaySpriteAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySpriteAnimation : MonoBehaviour {
+
+    public Animator anim;
+    public string animStateName;
+    public int animLayer = 0;
+
+    public void PlayAnim() {
+        Debug.Log("Bang");
+        anim.Play(animStateName, animLayer);
+    }
+}
